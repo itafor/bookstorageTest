c04096056e6488fbcf9c642ea0da5866e10638f0
francis <itaforfrancis@gmail.com>
2019-07-21 13:08:44 -0700

    update

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,14 @@ use Illuminate\Http\Request;
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
     Route::get('read-book/{bookId}', 'AuthController@readBook');
+    Route::get('all-books', 'AuthController@allBooks');
+
     Route::group([
       'middleware' => 'auth:api'
     ], function() {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
         Route::post('create-book', 'AuthController@addBooks');
-        Route::get('all-books', 'AuthController@allBooks');
         Route::post('update-book/{bookId}', 'AuthController@updateBook');
         Route::delete('delete-book/{bookId}', 'AuthController@destroyBook');
         Route::get('my-books', 'AuthController@myBooks');
